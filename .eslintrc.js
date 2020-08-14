f8ce7b3d12bad6599a9873c8cdc9b8b795a30c02
Kai Yuan Neo <kai@rocketacademy.co>
2020-08-14 22:45:51 +0800

    add eslint and gitignore to starter code

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    // Allow console for students to debug
+    'no-console': 'off',
+    // TODO(akira): Please clarify why we have this
+    'no-restricted-globals': 'off',
+    // Don't require vars to be declared at the top
+    'vars-on-top': 'off',
+    // Enable var instead of just let and const
+    'no-var': 'off',
+    // Enable + sign to concatenate strings
+    'prefer-template': 'off',
+    // Don't enforce ===
+    eqeqeq: 'off',
+    // Don't require a += b instead of a = a + b
+    'operator-assignment': 'off',
+    // Disable func-names rule so that we can have anonymous functions
+    'func-names': 'off',
+  },
+};
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var main = function(input) {
+// Disable ESLint rule no-unused-vars on next line because this code
+// is a learning tool and not a full-fledged node app.
+// eslint-disable-next-line no-unused-vars
+var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
