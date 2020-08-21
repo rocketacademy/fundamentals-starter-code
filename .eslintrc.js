78844b89c9caa93692dd81398203dbcf0724e28a
Kai Yuan Neo <kai@rocketacademy.co>
2020-08-21 16:33:17 +0800

    alphabetise eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,26 +10,26 @@ module.exports = {
     ecmaVersion: 11,
   },
   rules: {
-    // Allow console for students to debug
-    'no-console': 'off',
-    // Don't require vars to be declared at the top
-    'vars-on-top': 'off',
-    // Enable var instead of just let and const
-    'no-var': 'off',
-    // Enable + sign to concatenate strings
-    'prefer-template': 'off',
     // Don't enforce ===
     eqeqeq: 'off',
-    // Don't require a += b instead of a = a + b
-    'operator-assignment': 'off',
     // Disable func-names rule so that we can have anonymous functions
     'func-names': 'off',
     // Disable linebreak style to prevent ESLint errors on Windows line endings
     // https://eslint.org/docs/rules/linebreak-style
     'linebreak-style': 'off',
-    // Disable radix requirement for functions like parseInt
-    radix: 'off',
+    // Allow console for students to debug
+    'no-console': 'off',
     // Do not complain about unused main function
     'no-unused-vars': ['error', { varsIgnorePattern: 'main' }],
+    // Enable var instead of just let and const
+    'no-var': 'off',
+    // Don't require a += b instead of a = a + b
+    'operator-assignment': 'off',
+    // Enable + sign to concatenate strings
+    'prefer-template': 'off',
+    // Disable radix requirement for functions like parseInt
+    radix: 'off',
+    // Don't require vars to be declared at the top
+    'vars-on-top': 'off',
   },
 };
