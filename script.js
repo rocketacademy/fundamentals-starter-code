a5215c49fef074393f358c0accd6cfae8f50d59d
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-17 00:11:18 +0800

    fix instructions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
  * Instructions:
  * Each group of functions under a "/**" comment represents 1 exercise.
- * Uncomment each main function 1 by 1 (and comment out all others) to
+ * Uncomment each group of functions 1 by 1 (and comment out all others) to
  * execute the code for the relevant exercise.
  */
 
