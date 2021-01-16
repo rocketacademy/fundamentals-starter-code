f23f51d9ea23c6e898ee23132b9285dd8523cc34
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-16 22:44:55 +0800

    add instructions to script

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+ * Instructions:
+ * Each group of functions under a "/**" comment represents 1 exercise.
+ * Uncomment each main function 1 by 1 (and comment out all others) to
+ * execute the code for the relevant exercise.
+ */
+
+/**
+ * 4D
+ */
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
