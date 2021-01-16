e41f8c281a5b7bb9c240029bfb196ec551d0f8b6
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-16 22:40:56 +0800

    fix typo in comment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*
  * Instructions:
- * Each group fo functions under a "/**" comment represents 1 exercise.
+ * Each group of functions under a "/**" comment represents 1 exercise.
  * Uncomment each main function 1 by 1 (and comment out all others) to
  * execute the code for the relevant exercise.
  */
