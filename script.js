26538ce2146d45f683b4477a65177f497635caa4
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-16 16:11:20 +0800

    minutes in week

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
+// Minutes in a week
+var main = function (numWeeks) {
+  var daysPerWeek = 7;
+  var hoursPerDay = 24;
+  var minutesPerHour = 60;
+  var numMinutes = numWeeks * daysPerWeek * hoursPerDay * minutesPerHour;
+  return `In ${numWeeks} weeks there are ${numMinutes} minutes! Wow!`;
 };
