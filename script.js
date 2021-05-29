50583e41a690081c73746002081362a379b03510
jeremylim91 <jeremylim_91@live.com>
2021-05-29 11:23:24 +0800

    deleted extra main fn

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
