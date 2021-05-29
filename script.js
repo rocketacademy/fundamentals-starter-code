a633bf88d1cdc80f7765d18418e15fdb9316639f
jeremylim91 <jeremylim_91@live.com>
2021-05-29 11:20:59 +0800

    re-added main fn

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
+var main = function (input) {
+  var myOutputValue = 'hello world';
+  return myOutputValue;
+};
