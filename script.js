c6af0ca970eed259f6d5c750b0a4bdcb4493dbdb
jeremylim91 <jeremylim_91@live.com>
2021-06-13 17:59:29 +0800

    added red mode. ref soln completed

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+var mode = 'green';
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  if (input == 'greenmode') {
+    mode = 'green';
+  } else if (input == 'bluemode') {
+    mode = 'blue';
+  } else if (input == 'redmode') {
+    mode = 'red';
+  }
+
+  var myOutputValue =
+    'A fool sees not the same tree that a wise man sees. -William Blake';
+
+  if (mode == 'blue') {
+    myOutputValue =
+      'The sea, once it casts its spell, holds one in its net of wonder forever. -Jacques Cousteau';
+  }
+  if (mode == 'red') {
+    myOutputValue = '“A drop of ink may make a million think.” – Lord Byron';
+  }
+
   return myOutputValue;
 };
