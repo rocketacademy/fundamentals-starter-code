9cbfec9bc6988e6fa8164de6dd60da299953161b
jeremylim91 <jeremylim_91@live.com>
2021-06-14 11:05:55 +0800

    completed parts of the exercise i missed earlier: most rolled

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 var prevDiceRoll = 0;
 // Track the user's running score
 var score = 0;
+// Track number of games won
+var numGamesPlayed = 0;
+// Track number of losses
+var numGamesWon = 0;
+
+// Track the frequency of dice rolls for each dice value
+var diceVal1Freq = 0;
+var diceVal2Freq = 0;
+var diceVal3Freq = 0;
+var diceVal4Freq = 0;
+var diceVal5Freq = 0;
+var diceVal6Freq = 0;
 
 /**
  * Set a function that returns the value of a dice roll
@@ -13,7 +25,7 @@ var rollDice = function () {
   // take off the decimal
   var randomInteger = Math.floor(randomDecimal);
 
-  // it's anumber from 0 - 5 ... add 1
+  // it's a number from 0 - 5 ... add 1
   var diceNumber = randomInteger + 1;
 
   return diceNumber;
@@ -52,16 +64,109 @@ var determineScoreincrement = function (diceNum, userGuess) {
     return 4;
   }
 };
+/**
+ * Calculate how many times the user wins out of the total number of games he plays
+ */
+var getWinRateInfo = function () {
+  var winRate = Math.floor((numGamesWon / numGamesPlayed) * 100);
+  return 'You win ' + winRate + '% of the time.';
+};
+/**
+ * Increase the frequency of the relevant dice roll
+ * @param {number} diceRoll
+ */
+var incrDiceRollFreq = function (diceRoll) {
+  if (diceRoll == 1) {
+    diceVal1Freq += 1;
+  } else if (diceRoll == 2) {
+    diceVal2Freq += 1;
+  } else if (diceRoll == 3) {
+    diceVal3Freq += 1;
+  } else if (diceRoll == 4) {
+    diceVal4Freq += 1;
+  } else if (diceRoll == 5) {
+    diceVal5Freq += 1;
+  } else if (diceRoll == 6) {
+    diceVal6Freq += 1;
+  }
+};
+
+/*
+ * Set a fn that returns information about the most rolled dice val
+ */
+var getMostFreqRolled = function () {
+  if (
+    diceVal1Freq > diceVal2Freq &&
+    diceVal1Freq > diceVal3Freq &&
+    diceVal1Freq > diceVal4Freq &&
+    diceVal1Freq > diceVal5Freq &&
+    diceVal1Freq > diceVal6Freq
+  ) {
+    return 'You roll 1 most often';
+  } else if (
+    diceVal2Freq > diceVal1Freq &&
+    diceVal2Freq > diceVal3Freq &&
+    diceVal2Freq > diceVal4Freq &&
+    diceVal2Freq > diceVal5Freq &&
+    diceVal2Freq > diceVal6Freq
+  ) {
+    return 'You roll 2 most often';
+  } else if (
+    diceVal3Freq > diceVal1Freq &&
+    diceVal3Freq > diceVal2Freq &&
+    diceVal3Freq > diceVal4Freq &&
+    diceVal3Freq > diceVal5Freq &&
+    diceVal3Freq > diceVal6Freq
+  ) {
+    return 'You roll 3 most often';
+  } else if (
+    diceVal4Freq > diceVal1Freq &&
+    diceVal4Freq > diceVal2Freq &&
+    diceVal4Freq > diceVal3Freq &&
+    diceVal4Freq > diceVal5Freq &&
+    diceVal4Freq > diceVal6Freq
+  ) {
+    return 'You roll 4 most often';
+  } else if (
+    diceVal5Freq > diceVal1Freq &&
+    diceVal5Freq > diceVal2Freq &&
+    diceVal5Freq > diceVal3Freq &&
+    diceVal5Freq > diceVal4Freq &&
+    diceVal5Freq > diceVal6Freq
+  ) {
+    return 'You roll 5 most often';
+  } else if (
+    diceVal6Freq > diceVal1Freq &&
+    diceVal6Freq > diceVal2Freq &&
+    diceVal6Freq > diceVal3Freq &&
+    diceVal6Freq > diceVal4Freq &&
+    diceVal6Freq > diceVal5Freq
+  ) {
+    return 'You roll 6 most often';
+  }
+  // else handle scenario where there isn't a most frequently rolled dice num
+  return "You don't have a most-rolled dice";
+};
 
 /**
  * @param {string} input
  * Determine the score increment depending on how far off the guess was from the dice roll
  */
 var main = function (input) {
+  // Change user input from a string to a number; then, assign it to a variable so it's more intuitive to reference.
+  var userGuess = Number(input);
+
+  // increase the number of games played
+  numGamesPlayed += 1;
+
+  // roll the dice to get a value
   var randomDiceNumber = rollDice();
 
-  // assign input to userGuess so the name is more intuititive; change it from string to integer
-  var userGuess = Number(input);
+  // increase the frequency of the roll, depending on what randomDiceNumber is
+  incrDiceRollFreq(randomDiceNumber);
+
+  // Set the display message for the most frequently rolled dice
+  var mostFreqRolledDiceInfo = getMostFreqRolled();
 
   // Set a fn that returns a string containing information about the previous dice roll
   var prevDiceRollInfo = getPrevDiceRollInfo();
@@ -69,10 +174,16 @@ var main = function (input) {
   // update prevDiceRoll with the current dice roll
   prevDiceRoll = randomDiceNumber;
 
+  // Get display message about the win rate
+  var winRateInfo = getWinRateInfo();
+
   // Set the display message for the scenario where the user doesnt't win
   var myOutputValue =
+    winRateInfo +
+    mostFreqRolledDiceInfo +
+    '<br><br>' +
     prevDiceRollInfo +
-    ' You just rolled a ' +
+    'You just rolled a ' +
     randomDiceNumber +
     '. You guessed ' +
     input +
@@ -84,6 +195,11 @@ var main = function (input) {
     (userGuess >= randomDiceNumber && userGuess <= randomDiceNumber + 4) ||
     (userGuess <= randomDiceNumber && userGuess >= randomDiceNumber - 4)
   ) {
+    // Increase the number of games won
+    numGamesWon += 1;
+    // Recalculate the win rate and get a new display message about the win rate
+    winRateInfo = getWinRateInfo();
+
     // determine the increment amt and update the score accordingly
     var increment = determineScoreincrement(randomDiceNumber, userGuess);
     score += increment;
@@ -91,6 +207,9 @@ var main = function (input) {
     // Set the display message for the scenario where the user doesnt't win
     //Notice that we are re-assigning myOutputValue with a new value (i.e. a win msg) that replaces the old value (i.e. a lose msg). This is because line 73 alr assigns a value to myOutputValue, but now we want to replace that value since it is a win instead of a loss.
     myOutputValue =
+      winRateInfo +
+      mostFreqRolledDiceInfo +
+      '<br><br>' +
       prevDiceRollInfo +
       ' You just rolled a ' +
       randomDiceNumber +
