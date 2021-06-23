b43b61f32254f23c6b0f41654d3d72953c0212bc
jeremylim91 <jeremylim_91@live.com>
2021-06-23 19:10:40 +0800

    completed solns for base, excluding secret word

--- script.js
+++ script.js
@@ -25,14 +25,50 @@ var rollDice = function () {
 
 var main = function (input) {
   // Generate a random dice number
-  var randomDiceNumber = rollDice();
+  var randomDiceNumber = 5;
+  // var randomDiceNumber = rollDice();
 
   // Default output value is 'you lose'.
-  var myOutputValue = 'you lose';
+  var myOutputValue = 'You lose';
 
-  // User wins if dice roll is 2 times the user's guess. Update the output if so.
-  if (input == randomDiceNumber / 2) {
-    myOutputValue = 'you win';
+  // Updated Dice Game: User wins if his guess is +1 or -1 of the dice roll's value.
+  // if (
+  //   input == randomDiceNumber ||
+  //   input == randomDiceNumber + 1 ||
+  //   input == randomDiceNumber - 1
+  // ) {
+  //   myOutputValue = 'you win';
+  // }
+
+  // Easier Dice Game: User wins if his guess is + 2 or -2 of the dice roll's value
+  // if (
+  //   input == randomDiceNumber ||
+  //   input == randomDiceNumber + 1 ||
+  //   input == randomDiceNumber + 2 ||
+  //   input == randomDiceNumber - 1 ||
+  //   input == randomDiceNumber - 2
+  // ) {
+  //   myOutputValue = 'you win';
+  // }
+
+  // Even Easier Dice Game: User inputs 'odd' or 'even' and they win/lose depending on the whether the dice roll is odd or even
+  if (input == 'odd') {
+    if (
+      randomDiceNumber == 1 ||
+      randomDiceNumber == 3 ||
+      randomDiceNumber == 5
+    ) {
+      myOutputValue = 'You win';
+    }
+  }
+  if (input == 'even') {
+    if (
+      randomDiceNumber == 2 ||
+      randomDiceNumber == 4 ||
+      randomDiceNumber == 6
+    ) {
+      myOutputValue = 'You win';
+    }
   }
 
   // Return output.
