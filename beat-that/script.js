e58165109f64e4f35711ea99d0ccf10d3dbbf47e
tinsights <aj.tinaes@gmail.com>
2021-11-29 21:25:27 +0800

    input validation

--- beat-that/script.js
+++ beat-that/script.js
@@ -86,6 +86,10 @@ var main = function(input) {
   }
   if (gameState == 'CHOOSE') {
     console.log("Control flow checking: we are in choose mode.")
+    // input validation
+    if (input != 1 && input != 2) {
+      return 'Please input either 1 or 2';
+    }
     outputValue = playerChoice(input);
     return outputValue;
   }
