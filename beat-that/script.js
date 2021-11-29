973ece53c0b172fb02b4f3e1f1471ea5db6bb545
tinsights <aj.tinaes@gmail.com>
2021-11-29 21:22:53 +0800

    base complete, TODO: input validation

--- beat-that/script.js
+++ beat-that/script.js
@@ -1,32 +1,6 @@
-/*======= REQUIREMENTS ========
-1) There are 2 players and players take turns.
-2) When a player clicks Submit, the game rolls 2 dice and shows the dice rolls, for example 3 and 6.
-3) The player picks the order of the dice they want. For example, if they wanted the number 63, they would specify that the 2nd dice goes first. You can choose how the player specifies dice order.
-4) After both players have rolled and chosen dice order, the player with the higher combined number wins.
-*/
-
-/*===== Problem breakdown and planning =======
-1) First we will build a simple version that rolls 2 dice and returns the output for one player
-2) Then we will implement choosing dice order, for one player, and return the output
-3) Then we will try to re-factor the code to work for 2 players, and allow both players to choose the order and return the output
-4) Then we will try to implement comparing and deciding on the winner
-
-
-Tentative structure (open to changing during implementation):
-GLOBAL VARIABLES:
-1) Players dice roll
-2) Game State
-
-HELPER FUNCTIONS:
-1) rollDice
-2) playTurn(playerNumber) where playerNumber is either 1 or 2
-2) Checking for winner
-*/
-
-
 // initiliase an array to store dice rolls, we will use this for both player one and player two's dice rolls. player one's rolls will be index 0 and 1, player two's rolls will be indexed 2 and 3.
 var playerRolls = []
-// variable to track game state, initialised to ROLL, will change to CHOOSE
+// variable to track game state, initialised to ROLL, will change to CHOOSE, then to COMPARE
 var gameState = 'ROLL';
 // variable to track player's turn, initialised to 0
 var turnCounter = 0;
@@ -100,7 +74,7 @@ var main = function(input) {
     console.log("Control flow checking: we are in roll mode.")
     // if the second player just rolled, reset the array and counter 
     if (turnCounter == 2) { 
-      // reset playerRolls array to be empty
+      // reset global variables
       playerRolls = [];
       playersChosenNumbers = [];
       turnCounter = 0;
@@ -118,6 +92,7 @@ var main = function(input) {
   if (gameState == 'COMPARE') {
     console.log("Control flow checking: we are in compare mode");
     outputValue = compareNumbers();
+    gameState = 'ROLL';
     return outputValue;
   }
 }
