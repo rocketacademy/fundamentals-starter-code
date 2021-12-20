b954affebc015e8d0f0c5362fb01aea0b0ddc204
smdewi <smdewi2020@gmail.com>
2021-12-20 13:39:09 +0800

    Create helper function to make a deck of cards

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 //Blackjack game
 //Start game
+
 //Create a deck of card
+var makeDeck = function () {
+  var cardDeck = [];
+  var suits = ["diamonds", "hearts", "clubs", "spades"];
+
+  // Loop over the suits array
+  var suitIndex = 0;
+  while (suitIndex < suits.length) {
+    // Store the current suit in a variable
+    var currentSuit = suits[suitIndex];
+
+    var rankCounter = 1;
+    // Loop from 1 to 13 to create all cards for each suit
+    while (rankCounter <= 13) {
+      // By default, the card name is the same as rankCounter
+      var cardName = rankCounter;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName == 1) {
+        cardName = "ace";
+      } else if (cardName == 11) {
+        cardName = "jack";
+      } else if (cardName == 12) {
+        cardName = "queen";
+      } else if (cardName == 13) {
+        cardName = "king";
+      }
+
+      // Create a new card with the current name, suit, rank and value
+      var card = {
+        name: cardName,
+        suit: currentSuit,
+        rank: rankCounter,
+      };
+
+      // Add the new card to the deck
+      cardDeck.push(card);
+
+      // Increment rankCounter to iterate over the next rank
+      rankCounter += 1;
+    }
+
+    // Increment the suit index to iterate over the next suit
+    suitIndex += 1;
+  }
+
+  // Return the completed card deck
+  return cardDeck;
+};
+
 //Shuffle the deck
 //Deal cards to player and dealer
 //Give player option to hit or stand
@@ -9,6 +59,9 @@
 
 var main = function (input) {
   var myOutputValue = "hello world";
+
+  var newDeck = makeDeck();
+  console.log(newDeck);
 
   if (input == "hit") {
     myOutputValue = "The Hit Button has been clicked.";
