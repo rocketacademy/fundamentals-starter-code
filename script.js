c14cf59a1c14c322d6bdd9013216316a4ab32890
smdewi <smdewi2020@gmail.com>
2021-12-20 13:02:34 +0800

    Add pseudo codes

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+//Blackjack game
+//Start game
+//Create a deck of card
+//Shuffle the deck
+//Deal cards to player and dealer
+//Give player option to hit or stand
+//Calculate score
+//Determine who wins
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
